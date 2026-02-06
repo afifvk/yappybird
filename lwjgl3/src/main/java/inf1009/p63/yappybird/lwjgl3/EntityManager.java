72d9208233daf9499619106c8f08a7850b62ce88
kyhryley <kyhryley@gmail.com>
2026-02-06 13:37:51 +0800

    entity

--- lwjgl3/src/main/java/inf1009/p63/yappybird/lwjgl3/EntityManager.java
+++ lwjgl3/src/main/java/inf1009/p63/yappybird/lwjgl3/EntityManager.java
@@ -1,5 +1,31 @@
 package inf1009.p63.yappybird.lwjgl3;
 
+import java.util.ArrayList; 
+import java.util.Collections;
+import java.util.List;
+
+
 public class EntityManager {
 
+    private final List<Entity> entities;
+
+    public EntityManager() {
+        entities = new ArrayList<>();
+    }
+
+    public void addEntity(Entity entity) {
+        if (entity == null) {
+            throw new IllegalArgumentException("Entity cannot be null");
+        }
+        entities.add(entity);
+    }
+
+    public void delete(Entity deleteEntity) {
+        entities.remove(deleteEntity);
+    }
+
+    // UML name is getEntity(): List<Entity>
+    public List<Entity> getEntity() {
+        return Collections.unmodifiableList(entities);
+    }
 }
