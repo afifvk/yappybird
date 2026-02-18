39a2685b48281e2f689f9c8fd32c07585e427dd0
huanghuanna418 <huanghuanna418@gmail.com>
2026-02-18 11:42:28 +0800

    Update DropletScene.java
    - Popup label + timer
    - Calls to show popup
    - Calls to accuracy calculation
    - Calls to slowdown percentage calculation
    - Logic so only droplets are slowed

--- lwjgl3/src/main/java/inf1009/p63/yappybird/lwjgl3/DropletScene.java
+++ lwjgl3/src/main/java/inf1009/p63/yappybird/lwjgl3/DropletScene.java
@@ -14,249 +14,311 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class DropletScene extends Scene {
 
-    private Texture bucketImage;
-    private Texture dropImage;
-    private Texture backgroundTexture; // For the street scene
-    private Texture rainOverlayTexture;
-    private float bucketVelY = 0f;
-    private static final float GRAVITY = -650f;      // down
-    private static final float JUMP_SPEED = 450f;    // up
-    private static final float GROUND_Y = 20f; 
-    private Texture coinImage;
-    private TextureObject coin = null;
-    private float timeScale = 1f;
-    private boolean slowActive = false;
-    private float slowTimer = 0f;
-    private static final float SLOW_DURATION = 5f;
-    private long lastCoinTime = 0;
-    private static final long COIN_COOLDOWN = 5000000000L; // 5 seconds
+	private Texture bucketImage;
+	private Texture dropImage;
+	private Texture backgroundTexture; // For the street scene
+	private Texture rainOverlayTexture;
+	private float bucketVelY = 0f;
+	private static final float GRAVITY = -650f; // down
+	private static final float JUMP_SPEED = 450f; // up
+	private static final float GROUND_Y = 20f;
+	private Texture coinImage;
+	private TextureObject coin = null;
+	private float timeScale = 1f;
+	private boolean slowActive = false;
+	private float slowTimer = 0f;
+	private static final float SLOW_DURATION = 5f;
+	private long lastCoinTime = 0;
+	private static final long COIN_COOLDOWN = 5000000000L; // 5 seconds
 
-    //private Sound dropSound;
-    //private Sound rainMusic;
+	// private Sound dropSound;
+	// private Sound rainMusic;
 
-    private InputOutputManager inputManager;
-    private MovementManager movementManager;
-    private CollisionManager collisionManager;
-    private SceneManager sceneManager;
+	private InputOutputManager inputManager;
+	private MovementManager movementManager;
+	private CollisionManager collisionManager;
+	private SceneManager sceneManager;
 
-    private Stage stage;
-    private Label scoreLabel;
-    private Skin skin;
-    
-    
+	private Stage stage;
+	private Label scoreLabel;
+	private Skin skin;
 
-    
-    int dropsGathered = 0;
-    int dropsMissed = 0;
-    private float rainOffset = 0;
-    
-    // We need to keep track of the bucket specifically to move it
-    private TextureObject bucket;
-    private long lastDropTime;
+	// Popup UI (Help to show accuracy or slowdown for a short time)
+	private Label popupLabel;
+	private float popupTimer = 0f;
+	private static final float POPUP_DURATION = 2.0f;
 
-    public DropletScene(SceneManager sm) {
-        this.sceneManager = sm;
-    }
+	int dropsGathered = 0;
+	int dropsMissed = 0;
+	private float rainOffset = 0;
 
-    @Override
-    public void onEnter() {
+	// We need to keep track of the bucket specifically to move it
+	private TextureObject bucket;
+	private long lastDropTime;
 
-        this.inputManager = new InputOutputManager();
-        this.movementManager = new MovementManager();
-        // 1. Load the images
-        // NOTE: Make sure these names match your assets exactly!
-        bucketImage = new Texture(Gdx.files.internal("bucket.png"));
-        dropImage = new Texture(Gdx.files.internal("droplet.png")); // or water.png
-        backgroundTexture = new Texture(Gdx.files.internal("background.jpg")); // Make sure this exists!
-        rainOverlayTexture = new Texture(Gdx.files.internal("rain_overlay.jpg")); // Use JPG or PNG
-        coinImage = new Texture(Gdx.files.internal("coin.png"));
+	public DropletScene(SceneManager sm) {
+		this.sceneManager = sm;
+	}
 
-        // 2. Load sounds
-        SoundManager.getInstance(null).loadSound("drop", "droplet.mp3");
-    
-        // 2. Load background music (optional)
-        SoundManager.getInstance(null).loadMusic("rain", "rain.mp3");
-        SoundManager.getInstance(null).playMusic("rain", true); // true = loop
+	@Override
+	public void onEnter() {
 
-        // 3. Create the Bucket
-        // (Texture, x, y, width, height)
-        bucket = new TextureObject(bucketImage, 640 / 2 - 64 / 2, 20, 64, 64);
-        collisionManager = new CollisionManager();
-        
-        // 4. Add bucket to the Entity Manager so it gets drawn
-        this.entity.addEntity(bucket);
-        
-        // 5. Spawn the first raindrop
-        spawnRaindrop();
+		this.inputManager = new InputOutputManager();
+		this.movementManager = new MovementManager();
+		// 1. Load the images
+		// NOTE: Make sure these names match your assets exactly!
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+		dropImage = new Texture(Gdx.files.internal("droplet.png")); // or water.png
+		backgroundTexture = new Texture(Gdx.files.internal("background.jpg")); // Make sure this exists!
+		rainOverlayTexture = new Texture(Gdx.files.internal("rain_overlay.jpg")); // Use JPG or PNG
+		coinImage = new Texture(Gdx.files.internal("coin.png"));
 
-        // 6. Setup UI Stage for score display
-        stage = new Stage(new ScreenViewport());
-        
-        // 2. Get your Skin (same one used in Menu)
-        this.skin = SimpleSkin.getSkin();
-        
-        // 3. Create the Score Label
-        // "Score: 0" is the initial text
-        scoreLabel = new Label("Score: 0 | Missed: 0/10", skin);
-        
-        // 4. Position it (Top Left)
-        scoreLabel.setPosition(20, 480 - 40); // 20px from left, 40px from top
-        scoreLabel.setFontScale(1.5f); // Make it a bit bigger
-        
-        // 5. Add to Stage
-        stage.addActor(scoreLabel);
-    }
+		// 2. Load sounds
+		SoundManager.getInstance(null).loadSound("drop", "droplet.mp3");
 
-    private void trySpawnCoin() {
-        if (coin != null) return;
-        if (slowActive) return;
-        if (TimeUtils.nanoTime() - lastCoinTime < COIN_COOLDOWN) return;
-        if (MathUtils.random() < 0.98f) return;
+		// 2. Load background music (optional)
+		SoundManager.getInstance(null).loadMusic("rain", "rain.mp3");
+		SoundManager.getInstance(null).playMusic("rain", true); // true = loop
 
-        coin = new TextureObject(coinImage, MathUtils.random(0, 640 - 52), 480, 52, 52);
-        coin.setVelocity(0, -200);
-        this.entity.addEntity(coin);
-        lastCoinTime = TimeUtils.nanoTime();
-    }
+		// 3. Create the Bucket
+		// (Texture, x, y, width, height)
+		bucket = new TextureObject(bucketImage, 640 / 2 - 64 / 2, 20, 64, 64);
+		collisionManager = new CollisionManager();
 
-    @Override
-    public void update(float delta) {
-        // INPUT LOGIC 
-        inputManager.update(delta);
+		// 4. Add bucket to the Entity Manager so it gets drawn
+		this.entity.addEntity(bucket);
 
-        if (slowActive) {
-            slowTimer -= delta;
-            if (slowTimer <= 0f) {
-                slowActive = false;
-                timeScale = 1f;
-            }
-        }
+		// 5. Spawn the first raindrop
+		spawnRaindrop();
 
-        trySpawnCoin();
+		// 6. Setup UI Stage for score display
+		stage = new Stage(new ScreenViewport());
 
-        // Reset only X input each frame
-        bucket.velocity.x = 0;
+		// 2. Get your Skin (same one used in Menu)
+		this.skin = SimpleSkin.getSkin();
 
-        if(inputManager.isKeyPressed(Input.Keys.LEFT))  bucket.velocity.x -= 200;
-        if(inputManager.isKeyPressed(Input.Keys.RIGHT)) bucket.velocity.x += 200;
+		// 3. Create the Score Label
+		// "Score: 0" is the initial text
+		scoreLabel = new Label("Score: 0 | Missed: 0/10", skin);
 
-        //  JUMP TRIGGER only if on the ground.
-        boolean onGround = bucket.rectangle.y <= GROUND_Y + 0.5f;
-        if (onGround && Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
-            bucketVelY = JUMP_SPEED;
-        }
+		// 4. Position it (Top Left)
+		scoreLabel.setPosition(20, 480 - 40); // 20px from left, 40px from top
+		scoreLabel.setFontScale(1.5f); // Make it a bit bigger
 
-        // GRAVITY 
-        bucketVelY += GRAVITY * delta;
-        bucket.velocity.y = bucketVelY;
+		// 5. Add to Stage
+		stage.addActor(scoreLabel);
+		// Pop up label under the score
+		popupLabel = new Label("", skin);
+		popupLabel.setPosition(20, 480 - 70); // slightly below score line
+		popupLabel.setFontScale(1.2f);
+		stage.addActor(popupLabel);
 
-        // Keep bucket in bounds horizontally
-        movementManager.keepInBounds(bucket,640);
-        movementManager.update(this.entity.getEntities(), delta * timeScale);
+	}
 
-        // GROUND CLAMP (after movement)
-        if (bucket.rectangle.y < GROUND_Y) {
-            bucket.rectangle.y = GROUND_Y;
-            bucketVelY = 0f;
-            bucket.velocity.y = 0f;
-        }
+	// Show a message on screen for a short time
+	private void showPopup(String message) {
+		popupLabel.setText(message);
+		popupTimer = POPUP_DURATION;
+	}
 
-        //Rain animation effect
-        rainOffset -= 500* delta;
-        if(rainOffset < -480) rainOffset =0;
+	private void trySpawnCoin() {
+		if (coin != null)
+			return;
+		if (slowActive)
+			return;
+		if (TimeUtils.nanoTime() - lastCoinTime < COIN_COOLDOWN)
+			return;
+		if (MathUtils.random() < 0.98f)
+			return;
 
-        // SPAWN LOGIC 
-        if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
-        
-        handleCollisions();
+		coin = new TextureObject(coinImage, MathUtils.random(0, 640 - 52), 480, 52, 52);
+		coin.setVelocity(0, -200);
+		this.entity.addEntity(coin);
+		lastCoinTime = TimeUtils.nanoTime();
+	}
 
-        stage.act(delta);
-    }
+	@Override
+	public void update(float delta) {
+		// INPUT LOGIC
+		inputManager.update(delta);
 
-    private void handleCollisions(){
-        Iterator<Entity> iter = this.entity.getEntities().iterator();
-        while(iter.hasNext()) {
-            Entity e = iter.next();
+		// Pop up countdown
+		if (popupTimer > 0f) {
+			popupTimer -= delta;
+			if (popupTimer <= 0f) {
+				popupLabel.setText("");
+			}
+		}
 
-            if (e == coin) {
-                if (collisionManager.checkCollision(coin, bucket)) {
-                    slowActive = true;
-                    slowTimer = SLOW_DURATION;
-                    timeScale = 0.5f;
+		if (slowActive) {
+			slowTimer -= delta;
+			if (slowTimer <= 0f) {
+				slowActive = false;
+				timeScale = 1f;
+			}
+		}
 
-                    iter.remove();
-                    coin = null;
-                    continue;
-                } else if (coin.rectangle.y + coin.rectangle.height < 0) {
-                    iter.remove();
-                    coin = null;
-                    continue;
-                }
-            }
-            
-            // We only care about moving droplets, not the bucket
-            if(e instanceof TextureObject && e != bucket && e != coin) {
-                TextureObject drop = (TextureObject) e;
-                
-                // Check 1: Did we catch it?
-                if(collisionManager.checkCollision(drop, bucket)) {
-                    dropsGathered++; // Increase score
-                    scoreLabel.setText("Score: " + dropsGathered); //Print to UI 
-                    SoundManager.getInstance(null).playSound("drop");
-                    iter.remove(); // Remove it once!
-                }
-        
-                else if(drop.rectangle.y + 64 < 0) {
-                    dropsMissed++;
-                    scoreLabel.setText("Score: " + dropsGathered + " | Missed: " + dropsMissed + "/10");
-                    System.out.println("Missed: " + dropsMissed);
+		trySpawnCoin();
 
-                    // check if drops missed reached 10
-                    if(dropsMissed >=  10) {
-                        sceneManager.setScene(new GameOver(sceneManager, dropsGathered));
-                        return;
-                    }
-                    iter.remove();
-                }
-               
-            }
-        }
-    }
+		// Reset only X input each frame
+		bucket.velocity.x = 0;
 
-    @Override
-    public void render() {
+		if (inputManager.isKeyPressed(Input.Keys.LEFT))
+			bucket.velocity.x -= 200;
+		if (inputManager.isKeyPressed(Input.Keys.RIGHT))
+			bucket.velocity.x += 200;
 
-        // 1. Draw Background and Animated Rain
-        this.rendererManager.getBatch().begin();
-        // Draw static street scene
-        this.rendererManager.getBatch().draw(backgroundTexture, 0, 0, 640, 480);
-        // Draw looping rain overlay
-        this.rendererManager.getBatch().draw(rainOverlayTexture, 0, rainOffset, 640, 480);
-        this.rendererManager.getBatch().draw(rainOverlayTexture, 0, rainOffset + 480, 640, 480);
-        this.rendererManager.getBatch().end();
+		// JUMP TRIGGER only if on the ground.
+		boolean onGround = bucket.rectangle.y <= GROUND_Y + 0.5f;
+		if (onGround && Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+			bucketVelY = JUMP_SPEED;
+		}
 
-        // Ask the Manager to draw everything in our list
-        this.rendererManager.render(this);
-        stage.draw();
-    }
+		// GRAVITY
+		bucketVelY += GRAVITY * delta;
+		bucket.velocity.y = bucketVelY;
 
-    @Override
-    public void onExit() {
-        // Clean up memory
-        if(bucketImage != null) bucketImage.dispose();
-        if(dropImage != null) dropImage.dispose();
-        if(coinImage != null) coinImage.dispose();
-        if(backgroundTexture != null) backgroundTexture.dispose();
-        if(rainOverlayTexture != null) rainOverlayTexture.dispose();
-        if(stage != null) stage.dispose();
-        if(skin != null) skin.dispose();
-    }
-    
-    // Helper function to create a drop
-    private void spawnRaindrop() {
-        TextureObject drop = new TextureObject(dropImage, MathUtils.random(0, 640-64), 480, 64, 64);
-        drop.setVelocity(0,-200);
-        this.entity.addEntity(drop);
-        lastDropTime = TimeUtils.nanoTime();
-    }
+		// Keep bucket in bounds horizontally
+		movementManager.keepInBounds(bucket, 640);
+		// Bucket, Coins moves normally.
+		// Droplet get slowed using timeScale.
+		for (Entity e : this.entity.getEntities()) {
+			if (e instanceof iMovable) {
+				if (e == bucket || e == coin) {
+					((iMovable) e).movement(delta);
+				} else {
+					// these are droplets
+					((iMovable) e).movement(delta * timeScale);
+				}
+			}
+		}
+
+		// GROUND CLAMP (after movement)
+		if (bucket.rectangle.y < GROUND_Y) {
+			bucket.rectangle.y = GROUND_Y;
+			bucketVelY = 0f;
+			bucket.velocity.y = 0f;
+		}
+
+		// Rain animation effect
+		rainOffset -= 500 * delta;
+		if (rainOffset < -480)
+			rainOffset = 0;
+
+		// SPAWN LOGIC
+		if (TimeUtils.nanoTime() - lastDropTime > 1000000000)
+			spawnRaindrop();
+
+		handleCollisions();
+
+		stage.act(delta);
+	}
+
+	private void handleCollisions() {
+		Iterator<Entity> iter = this.entity.getEntities().iterator();
+		while (iter.hasNext()) {
+			Entity e = iter.next();
+
+			if (e == coin) {
+				if (collisionManager.checkCollision(coin, bucket)) {
+					slowActive = true;
+					slowTimer = SLOW_DURATION;
+					timeScale = 0.5f;
+
+					// Show slowdown popup
+					float slowPct = collisionManager.timeScaleToSlowdownPercent(timeScale);
+					showPopup(collisionManager.buildSlowdownPopupText(slowPct));
+
+					iter.remove();
+					coin = null;
+					continue;
+				}
+
+				else if (coin.rectangle.y + coin.rectangle.height < 0) {
+					iter.remove();
+					coin = null;
+					continue;
+				}
+			}
+
+			// We only care about moving droplets, not the bucket
+			if (e instanceof TextureObject && e != bucket && e != coin) {
+				TextureObject drop = (TextureObject) e;
+
+				// Check 1: Did we catch it?
+				if (collisionManager.checkCollision(drop, bucket)) {
+
+					// 1) Score changes
+					dropsGathered++;
+					scoreLabel.setText("Score: " + dropsGathered + " | Missed: " + dropsMissed + "/10");
+					SoundManager.getInstance(null).playSound("drop");
+
+					// 2) Accuracy popup
+					float acc = collisionManager.calculateCatchAccuracyPercent(drop, bucket);
+					showPopup(collisionManager.buildAccuracyPopupText(acc));
+
+					// 3) Remove droplet after catching
+					iter.remove();
+				}
+
+				else if (drop.rectangle.y + 64 < 0) {
+					dropsMissed++;
+					scoreLabel.setText("Score: " + dropsGathered + " | Missed: " + dropsMissed + "/10");
+					System.out.println("Missed: " + dropsMissed);
+
+					// check if drops missed reached 10
+					if (dropsMissed >= 10) {
+						sceneManager.setScene(new GameOver(sceneManager, dropsGathered));
+						return;
+					}
+					iter.remove();
+				}
+
+			}
+		}
+	}
+
+	@Override
+	public void render() {
+
+		// 1. Draw Background and Animated Rain
+		this.rendererManager.getBatch().begin();
+		// Draw static street scene
+		this.rendererManager.getBatch().draw(backgroundTexture, 0, 0, 640, 480);
+		// Draw looping rain overlay
+		this.rendererManager.getBatch().draw(rainOverlayTexture, 0, rainOffset, 640, 480);
+		this.rendererManager.getBatch().draw(rainOverlayTexture, 0, rainOffset + 480, 640, 480);
+		this.rendererManager.getBatch().end();
+
+		// Ask the Manager to draw everything in our list
+		this.rendererManager.render(this);
+		stage.draw();
+	}
+
+	@Override
+	public void onExit() {
+		// Clean up memory
+		if (bucketImage != null)
+			bucketImage.dispose();
+		if (dropImage != null)
+			dropImage.dispose();
+		if (coinImage != null)
+			coinImage.dispose();
+		if (backgroundTexture != null)
+			backgroundTexture.dispose();
+		if (rainOverlayTexture != null)
+			rainOverlayTexture.dispose();
+		if (stage != null)
+			stage.dispose();
+		if (skin != null)
+			skin.dispose();
+	}
+
+	// Helper function to create a drop
+	private void spawnRaindrop() {
+		TextureObject drop = new TextureObject(dropImage, MathUtils.random(0, 640 - 64), 480, 64, 64);
+		drop.setVelocity(0, -200);
+		this.entity.addEntity(drop);
+		lastDropTime = TimeUtils.nanoTime();
+	}
 }
